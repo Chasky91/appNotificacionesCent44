f012f23c61a83a19c5e676faa647e7a60ec653a4
Chasky91 <david_c91@outlook.com>
2025-06-29 18:47:25 -0300

    completo

--- config.db.js
+++ config.db.js
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export const config = {
   host: process.env.HOST,
-  port: 25559,
+  port: process.env.PORT,
   user: 'root',
   password: process.env.DB_PASS,
   database: 'cent_44',
